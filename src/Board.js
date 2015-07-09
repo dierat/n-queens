cf018939131a1a01b4c77c7191de034ee8c6646f
dierat <diedra.rater@gmail.com>
2015-07-09 08:46:19 -0700

    adds working helper functions

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,42 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
+    
+      //get row from board (this)
+      var board = this.rows();
+      var currentRow = board[rowIndex];
+      //define variable counter and set to false 
+      var bound = false;
+      //loop through each index in row
+      for(var i = 0; i < currentRow.length; i++){
+        //check what value is in index
+        //if index is 1 and counter is true
+        if(currentRow[i] === 1){
+          if(bound){
+            return true;
+          //return true
+          } else {
+          //set bound to true
+            bound = true;
+          }//else
+        }//if
+      }//for 
       return false; // fixme
+    
     },
-
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      // grab table
+      var board = this.rows();
+      // iterate through all the rows
+      for (var i=0; i<board.length; i++){
+        // call previous function on each value
+        // if true
+        if ( this.hasRowConflictAt(i) ){
+          // return true
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -94,11 +125,45 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      // call board
+      var board = this.rows();
+      // declare variable found, set to false
+      var found = false;
+      // iterate through board, one row at a time
+      for (var i=0; i<board.length; i++){
+        // check colIndex in row if it's one
+        if (board[i][colIndex]){
+          // if found is true
+          if (found){
+            // return true
+            return true;
+          } else {
+          // else
+            // set found to true
+            found = true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      //declare variable, store boardj
+      var board = this.rows();
+      // if board has no rows
+      if (!board.length){
+        // reurn false
+        return false;
+      }
+      //loop through all index in first row
+      for(var i = 0; i < board[0].length; i++){
+        //if statement - hasColConflictAt index in loop
+        if(this.hasColConflictAt(i)){
+          //return true if true
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -108,12 +173,42 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+    // colIndex = majorDiagonalColumnIndexAtFirstRow
+    hasMajorDiagonalConflictAt: function(colIndex) {
+      // pull out the board
+      var board = this.rows();
+      // variable called found set to false
+      var found = false;
+      // loop through rows, set i to 0, max should be less than board.length - colIndex, incrementing
+      for (var i=0; i<board.length - colIndex; i++){
+        // check board[i][colIndex + i] is 1
+        if (board[i][colIndex + i]){
+          // if found is true
+          if (found){
+            // return true
+            return true;
+          // else 
+          } else {
+            // set found to true 
+            found = true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      // pull out ye olde board
+      var board = this.rows();
+      // loop through rows
+      for (var i=0; i<board.length; i++){
+        // for each square, check if hasMajorDiagonalConflictAt
+        if ( this.hasMajorDiagonalConflictAt(i) ){
+          // return true
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -123,12 +218,41 @@
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
-    hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
+    // colIndex = minorDiagonalColumnIndexAtFirstRow
+    hasMinorDiagonalConflictAt: function(colIndex) {
+      var board = this.rows();
+      // variable called found set to false
+      var found = false;
+      // loop through rows, set i to 0, max should be less than board.length - colIndex, incrementing
+      for (var i = 0; i <= colIndex; i++){
+        // check board[i][colIndex + i] is 1
+        if (board[i][colIndex - i]){
+          // if found is true
+          if (found){
+            // return true
+            return true;
+          // else 
+          } else {
+            // set found to true 
+            found = true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
+      // pull out ye olde board
+      var board = this.rows();
+      // loop through rows
+      for (var i=0; i<board.length; i++){
+        // for each square, check if hasMajorDiagonalConflictAt
+        if ( this.hasMinorDiagonalConflictAt(i) ){
+          // return true
+          return true;
+        }
+      }
       return false; // fixme
     }
 
